2a417d2940b2551583a87cfa03c9b1316d14f097
AndreyRedWhite <andreyredwhite@gmail.com>
2024-07-16 03:00:15 +0300

    started to code telegram bot

---
+++ health_checker.py
@@ -0,0 +1,78 @@
+import logging
+import subprocess
+import re
+
+
+def check_status() -> bool:
+    """
+       Checks if a process with the name 'arbitrage_bot.py' is running.
+
+       Returns:
+       - bool: True if the process is running, False otherwise.
+       """
+
+    try:
+        result = subprocess.run('ps -ax | grep arbitrage_bot.py', shell=True, stdout=subprocess.PIPE, text=True)
+        processes = result.stdout.split('\n')
+        regex = re.compile('[p|P]ython\d? arbitrage_bot\.py')
+        for process in processes:
+            if regex.search(process):
+                return True
+        return False
+    except Exception as e:
+        logging.error(f"An error occured. Here is it:\n{e}")
+        return False
+
+
+def get_last_n_log(n: int, date_only: bool = False) -> str:
+    """
+       Gets the last n lines from the arbitrage_bot.log file.
+       If date_only is True, returns only the date and time from each line.
+
+       Args:
+       - n (int): The number of lines to retrieve.
+       - date_only (bool): If True, returns only the date and time from each line.
+
+       Returns:
+       - str: The last n lines from the log file, or only the date and time from each line if date_only is True.
+       """
+
+    try:
+        result = subprocess.run(f'tail -n {n} logs/arbitrage_bot.log', shell=True, stdout=subprocess.PIPE, text=True)
+        log_lines = result.stdout.strip().split('\n')
+        if date_only:
+            date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}')
+            log_lines = [date_pattern.match(line).group(0) for line in log_lines if date_pattern.match(line)]
+        return '\n'.join(log_lines)
+    except Exception as e:
+        logging.error(f"An error occured. Here is it:\n{e} ")
+        return ""
+
+
+def get_oppotunities(last_lines: int = False):
+    """
+    Reads the file arbitrage_oppotunities.txt and returns its output
+
+    Args:
+    - last_lines(int): if set, returns only last n lines from the file
+
+    :return:
+    - str: an output from the arbitrage_oppotinities.txt file
+    """
+
+    try:
+        result = subprocess.run('cat arbitrage_opportunities.txt', shell=True, stdout=subprocess.PIPE, text=True)
+        opps = result.stdout.strip().split('\n')
+        if last_lines:
+            lines = [line for line in opps[-1: last_lines]]
+            return '\n'.join(lines)
+        return result.stdout.strip()
+    except Exception as e:
+        logging.error(f"An error occured. Here is it:\n{e} ")
+        return ""
+
+
+if __name__ == '__main__':
+    # print(check_status())
+    # print(get_last_n_log(3, date_only=True))
+    print(get_oppotunities(last_lines=2))
